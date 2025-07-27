8a86df54decf47df0c109c1cb9380a31590ef9d6
Vighnesh-code <vighneshzagade4@gmail.com>
2025-07-27 16:16:59 +0530

    Few homepage UI changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="h-screen w-screen">
-      <h1 className="text-4xl ">Hello from the Movie App</h1>
-      <h2></h2>
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h3 className="text-white text-5xl">{searchTerm}</h3>
+      </div>
+    </main>
   );
 }
 
