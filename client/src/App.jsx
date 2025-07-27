e9ec945480e6b69111f9786e7f91d36a4ea5327f
Vighnesh-code <vighneshzagade4@gmail.com>
2025-07-27 10:54:19 +0530

    basic moviyum setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <h1 className="text-4xl ">Hello from the Movie App</h1>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default App;
