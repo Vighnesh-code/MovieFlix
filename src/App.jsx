951f80c1173540f4e4e855cfcf21904049e7724b
Vighnesh-code <vighneshzagade4@gmail.com>
2025-08-15 16:19:04 +0530

    few more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="h-screen w-screen">
-      <h1 className="text-4xl">Hello World!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero-banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle.
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   );
 }
 
